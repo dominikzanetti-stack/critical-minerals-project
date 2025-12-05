b7d3ee4287ce59b94649012975dee06b905db1ab
Dominik Zanetti <dominikzanetti@gmail.com>
2025-12-05 15:44:46 +0100

    added tungsten and st.session_state logic

--- app.py
+++ app.py
@@ -4,25 +4,38 @@ import glob
 
 st.set_page_config(layout = "wide")
 st.title("Critical Minerals Interactive Table")
-
-filenames = glob.glob("*.csv")
-
-data ={}
-for filename in filenames:
-    clean_name = filename.replace(".csv", "")
-    data[clean_name] = pd.read_csv(filename, sep =";")
-
-elements = data["elements"]
-# Create data for the periodic table
-for r in range(1, 10):
-    cols = st.columns(18)
-    for c in range(1, 19):
-        
-        element_data = elements[(elements["Row"] == r) & (elements["Column"] == c)]
-        
-        if not element_data.empty:
-            cols[c-1].button(element_data.iloc[0]['Symbol'])
-            
 st.subheader("Raw data")
+
+if "selected_element" not in st.session_state:
+    st.session_state["selected_element"] = None
+
+if st.session_state["selected_element"] is None:
+    filenames = glob.glob("*.csv")
+
+    data ={}
+    for filename in filenames:
+        clean_name = filename.replace(".csv", "")
+        data[clean_name] = pd.read_csv(filename, sep =";")
+
+    elements = data["elements"]
+    # Create data for the periodic table
+    for r in range(1, 10):
+        cols = st.columns(18)
+        for c in range(1, 19):
+            
+            filter = (elements['Row'] == r) & (elements['Column'] == c)
+            element_data = elements[filter]
+            
+            if not element_data.empty:
+                symbol = element_data.iloc[0]['Symbol']
+
+                if cols[c-1].button(symbol):
+                    st.session_state["selected_element"] = symbol
+                    st.rerun()
+else:
+    st.write(st.session_state["selected_element"])
+
+
+
 st.write(data["elements"])
-st.write(data["prices"])
+st.write(f"Debug: You selected {st.session_state['selected_element']}")
