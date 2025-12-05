b216bb97db6d8232685beb693b489d8daed2a2f8
Dominik Zanetti <dominikzanetti@gmail.com>
2025-12-05 14:20:49 +0100

    Fix CSV separator to ; and adding periodic table grid

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import streamlit as st
 import pandas as pd
+import glob
 
 st.set_page_config(layout = "wide")
-
 st.title("Critical Minerals Interactive Table")
-st.write("This application will visualize critical minerals and their supply chains")
 
-data = pd.DataFrame({
-    'Element': ['Lithium', 'Cobalt', ' Nickel'],
-    'Price': ['100', '50', '20']
-})
+filenames = glob.glob("*.csv")
 
-st.dataframe(data)
+data ={}
+for filename in filenames:
+    clean_name = filename.replace(".csv", "")
+    data[clean_name] = pd.read_csv(filename, sep =";")
+
+elements = data["elements"]
+# Create data for the periodic table
+for r in range(1, 10):
+    cols = st.columns(18)
+    for c in range(1, 19):
+        
+        element_data = elements[(elements["Row"] == r) & (elements["Column"] == c)]
+        
+        if not element_data.empty:
+            cols[c-1].button(element_data.iloc[0]['Symbol'])
+            
+st.subheader("Raw data")
+st.write(data["elements"])
+st.write(data["prices"])
